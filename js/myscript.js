095532f5a480e2f0c305e301928a28bae7411a06
miura <miura@qu-bic.jp>
2021-02-26 19:38:36 +0900

    店舗情報：作成中（pc）

--- js/myscript.js
+++ js/myscript.js
@@ -38,6 +38,7 @@ $(function () {
 		slidesPerView: 1.4,
 		centeredSlides: true,
 		freeMode: true,
+		simulateTouch: false,
 		autoplay: {
 			delay: 1
 		}
@@ -48,12 +49,48 @@ $(function () {
 		slidesPerView: 6.5,
 		centeredSlides: true,
 		freeMode: true,
+		simulateTouch: false,
 		autoplay: {
 			delay: 1
 		}
 	});
+	var swiper03 = new Swiper('.school03 .swiper-container', {
+		loop: true,
+		speed: 1000,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		centeredSlides: true,
+		freeMode: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: true
+		},
+		navigation: {
+			nextEl: '.school03 .swiper-button-next',
+			prevEl: '.school03 .swiper-button-prev',
+		}
+	});
 
-
+	// スムーススクロール
+	var headerHeight = $('header').outerHeight();
+	var urlHash = location.hash;
+	if(urlHash) {
+			$('body,html').stop().scrollTop(0);
+			setTimeout(function(){
+					var target = $(urlHash);
+					var position = target.offset().top - headerHeight;
+					$('body,html').stop().animate({scrollTop:position}, 1000);
+			}, 100);
+	}
+	$('a[href^="#"]').click(function () {
+		var speed = 300;
+		var header = $('header').innerHeight();
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({ scrollTop: position - header }, speed, "swing");
+		return false;
+	});
 
 
 	// 一番最後に記述
